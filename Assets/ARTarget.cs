3fb2dd9a7da050df1b54b65c398efda69cf6b56b
vitalight <904844050@qq.com>
2019-01-14 10:29:14 +0800

    AR/burner: always render

---
+++ Assets/ARTarget.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARTarget : DefaultTrackableEventHandler {
+    protected override void OnTrackingFound() {
+        // do nothing
+    }
+
+    protected override void OnTrackingLost() {
+        // do nothing
+    }
+}
