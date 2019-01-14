6fa04c69c8653f49dffd87308d0e4b4ba53274ad
vitalight <904844050@qq.com>
2019-01-14 10:51:45 +0800

    burner: smooth movement

---
+++ Assets/ARFollower.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARFollower : MonoBehaviour {
+
+    public Transform Leader;
+
+    public float LerpSpeed = 0.2f;
+
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, Leader.position, LerpSpeed);
+        transform.rotation = Quaternion.Lerp(transform.rotation, Leader.rotation, LerpSpeed);
+	}
+}
